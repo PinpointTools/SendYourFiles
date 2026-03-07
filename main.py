034c589cb2d90a54e055a3c6376f33d8dfe24271
Daveberry <84567826+daveberrys@users.noreply.github.com>
2026-03-08 07:41:26 +0800

    fix frontend

--- main.py
+++ main.py
@@ -1,17 +1,9 @@
-from src.front.window import startUp as s
+from src.front.web import startWeb
 from src.back.system.history import History
 from src.back.system.settings import Settings
 
-import argparse
-import sys
-
-parser = argparse.ArgumentParser()
-parser.add_argument("-d", "--debug", action="store_true", help="enable debug mode")
-args = parser.parse_args()
-
-debugMode = args.debug
 History().checkHistory()
 Settings().checkFile()
 
 if __name__ == "__main__":
-    s(debugMode=debugMode)
+    startWeb()
---
+++ src/front/web.py
@@ -0,0 +1,19 @@
+import flask as f
+import src.back.util.print as print
+
+app = f.Flask(
+    __name__,
+    template_folder="main",
+    static_folder="main"
+)
+
+@app.route("/")
+def index():
+    return f.render_template("index.html")
+
+@app.route("/popup/<path:filename>")
+def popup(filename):
+    return f.send_from_directory("main/popup", filename)
+
+def startWeb():
+    app.run(debug=True)
